fef8cd7cede160675f43f3072b5685ead03220a9
sivarajan2003 <sivarajan2.21msc@kongu.edu>
2026-06-05 10:05:37 +0530

    dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -271,20 +271,15 @@ inactive={stats.inactiveSubjects || 0}
       </div>
 
       {/* EARNINGS + NOTICE + FEES */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-6">
-        <div className="lg:col-span-1">
-          <EarningsExpenses />
-        </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-6">
+  <div className="lg:col-span-2">
+    <NoticeBoard />
+  </div>
 
-        <div className="lg:col-span-2">
-          <NoticeBoard />
-        </div>
-
-        <div className="lg:col-span-1">
-          <FeeSummaryCards />
-        </div>
-      </div>
-
+  <div className="lg:col-span-1">
+    <FeeSummaryCards />
+  </div>
+</div>
       {/* QUICK ACTIONS */}
       <div className="mb-6">
         <QuickActionCards />
